ff80dc76e243969f1d79340ed7ee28045fdffcb8
Nino Bozzi <nino@bwired.ca>
2018-12-05 14:41:33 -0300

    After PortalOC scaffolding

---
+++ portaloc/portal_oc/models/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class portal_oc(models.Model):
+#     _name = 'portal_oc.portal_oc'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
