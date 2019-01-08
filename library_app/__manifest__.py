36f4e529f18a2af6c5dce300765d5673e2f20552
Nino Bozzi <n.bozzi@mbmlatam.cl>
2019-01-08 13:04:58 -0300

    fist manual scaffolding

---
+++ library_app/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Library Management',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+}
