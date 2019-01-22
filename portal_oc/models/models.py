134ddc1a82e79751c543b3be5d7b2bf868db7a55
Nino Bozzi <n.bozzi@mbmlatam.cl>
2019-01-22 15:43:03 -0300

    added models for sale

--- portal_oc/models/models.py
+++ portal_oc/models/models.py
@@ -11,18 +11,31 @@ class MbMStage(models.Model):
        fold = fields.Boolean()
        active = fields.Boolean(default=True)
        state = fields.Selection(
-           [('new','Borrador'),
-            ('sent','Enviada por R3'),
-            ('confirmed','Confirmada por MbM'),
-            ('process','En proceso de entrega'),
-            ('delivered','Entregada'),
-            ('invoiced','Factura emitida'),
-            ('paid', 'Pagado')],
+           [('new','New'),
+            ('open','Open'),
+            ('done','Paid'),
+            ('cancel', 'Cancelled')],
            default='new',
        )
 
+
 class PurchaseOrderMbM(models.Model):
     _inherit = 'purchase.order'
+    @api.model
+    def _default_stage(self):
+        Stage = self.env['portal_oc.stage']
+        return Stage.search([], limit=1)
+    @api.model
+    def _group_expand_stage_id(self, stages, domain, order):
+        return stages.search([], order=order)
+    stage_id = fields.Many2one(
+        'portal_oc.MbMStage',
+        default=_default_stage,
+        group_expand='_group_expand_stage_id')
+    state = fields.Selection(related='stage_id.state')
+
+class SaleOrderMbM(models.Model):
+    _inherit = 'sale.order'
     @api.model
     def _default_stage(self):
         Stage = self.env['portal_oc.stage']
