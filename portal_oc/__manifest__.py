395f857912c77a9f2cbfec85f77e353ef6a7fa37
Nino Bozzi <n.bozzi@mbmlatam.cl>
2019-01-22 16:25:58 -0300

    rollback due install error

--- portal_oc/__manifest__.py
+++ portal_oc/__manifest__.py
@@ -26,7 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'data/portal_oc_stages',
+        #'data/portal_oc_stages',
     ],
     # only loaded in demonstration mode
     'demo': [
