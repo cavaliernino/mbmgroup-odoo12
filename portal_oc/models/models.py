4b08692b7f476f4e6e3af761afa95b10c4c4e226
Nino Bozzi <n.bozzi@mbmlatam.cl>
2019-01-22 15:23:33 -0300

    agregado stages al modelo

--- portal_oc/models/models.py
+++ portal_oc/models/models.py
@@ -2,6 +2,39 @@
 
 from odoo import models, fields, api
 
+class MbMStage(models.Model):
+       _name = 'portal_oc.stage'
+       _description = 'MbM Stage'
+       _order = 'sequence,name'
+       name = fields.Char()
+       sequence = fields.Integer(default=10)
+       fold = fields.Boolean()
+       active = fields.Boolean(default=True)
+       state = fields.Selection(
+           [('new','Borrador'),
+            ('sent','Enviada por R3'),
+            ('confirmed','Confirmada por MbM'),
+            ('process','En proceso de entrega'),
+            ('delivered','Entregada'),
+            ('invoiced','Factura emitida'),
+            ('paid', 'Pagado')],
+           default='new',
+       )
+
+class PurchaseOrderMbM(models.Model):
+    _inherit = 'purchase.order'
+    @api.model
+    def _default_stage(self):
+        Stage = self.env['portal_oc.stage']
+        return Stage.search([], limit=1)
+    @api.model
+    def _group_expand_stage_id(self, stages, domain, order):
+        return stages.search([], order=order)
+    stage_id = fields.Many2one(
+        'portal_oc.MbMStage',
+        default=_default_stage,
+        group_expand='_group_expand_stage_id')
+    state = fields.Selection(related='stage_id.state')
 
 # class portal_oc(models.Model):
 #     _name = 'portal_oc.portal_oc'
